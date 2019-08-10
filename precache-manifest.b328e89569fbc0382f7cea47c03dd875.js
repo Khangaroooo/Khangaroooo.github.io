938e4b57726a6164e108fed2035dc44e3562b083
KhangNguyen3000 <nguyenh.khang.1999@gmail.com>
2019-08-10 04:59:35 -0400

    Updates

--- precache-manifest.b328e89569fbc0382f7cea47c03dd875.js
+++ precache-manifest.b328e89569fbc0382f7cea47c03dd875.js
@@ -1,18 +1,18 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "b116e63cd46d7eb5da9ee753b25c92e7",
+    "revision": "bfdc50b00aa946dadccc5c6683ae0f24",
     "url": "/KhangNguyen3000.github.io/index.html"
   },
   {
-    "revision": "4c3692735dafdbef99d0",
-    "url": "/KhangNguyen3000.github.io/static/css/main.b3f75e8e.chunk.css"
+    "revision": "176490cefe646a600d62",
+    "url": "/KhangNguyen3000.github.io/static/css/main.43d5208c.chunk.css"
   },
   {
     "revision": "39d12d597ecf44ce6979",
     "url": "/KhangNguyen3000.github.io/static/js/2.e6ba21d2.chunk.js"
   },
   {
-    "revision": "4c3692735dafdbef99d0",
+    "revision": "176490cefe646a600d62",
     "url": "/KhangNguyen3000.github.io/static/js/main.4f376ce8.chunk.js"
   },
   {
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/KhangNguyen3000.github.io/precache-manifest.be8079b17202ab1eff8300f7129ee885.js"
+  "/KhangNguyen3000.github.io/precache-manifest.b328e89569fbc0382f7cea47c03dd875.js"
 );
 
 self.addEventListener('message', (event) => {
