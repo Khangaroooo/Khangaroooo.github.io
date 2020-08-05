4687c05f6ca4589a210f4b864baf0f7c0600f4a3
Khang Nguyen <nguyenh.khang.1999@gmail.com>
2020-08-05 00:29:09 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e5da0385150a9d9933c4efe6fc92ad8e.js"
+  "/precache-manifest.0d60d79fcd0ec0dca3c80bbfcd7ed8ef.js"
 );
 
 self.addEventListener('message', (event) => {
